f66cbb1feb3556bb01f03cff625b9b549d2079f5
Michael Godfrey <m.godfrey59@gmail.com>
2023-04-19 20:23:38 +1200

    second commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Link from 'next/link';
-
-const categories = [{name: 'Cat 1', slug: 'cat1'}, {name: 'Cat 2', slug: 'cat2'}, {name: 'Cat 3', slug: 'cat3'}]
+import { getCategories } from '../services';
 
 const Header = () => {
+    const [categories, setCategories] = useState([]);
+
+      useEffect(() => {
+        getCategories()
+        .then((newCategories) => setCategories(newCategories));
+    }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
         <div className="border-b w-full inline-block border-blue-400 py-8">
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+  const [recentPosts, setRecentPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug)
+        .then((result) => setRelatedPosts(result));
+    } else {
+      getRecentPosts()
+        .then((result) => setRecentPosts(result));
+    }
+  }, [slug]);
+
   return (
-    <div>PostWidget</div>
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-b font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
+      {recentPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img src={post.featuredImage.url} alt={post.title} height="60px" width="60px" className="align-middle rounded-full" />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">{moment(post.createdAt).format('DD MMM YYYY')}</p>
+          </div>
+          <Link href={`/post/${post.slug}`} key={post.title} className="text-md">
+            {post.title}
+          </Link>
+        </div>
+      ))}
+    </div>
   )
 }
 
